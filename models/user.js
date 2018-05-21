6fed89dfecc862fc8626200f07e06564962f6efd
neilxcode <developros@icloud.com>
2018-05-21 03:53:49 -0400

    Create user model.

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {type: String, unique: true, required: true},
+  password: {type: String, required: true},
+  email:    {type: String, unique: true, required: true},
+  img:      {data: Buffer, contentType: String},
+  bio:      {type: String},
+  balance:  {type: Number}
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
