4f2b31b1026c2421906f3986ed6cae6f49896a7f
Jens Kai <jkaih7@gmail.com>
2018-05-29 12:23:51 -0400

    default photo for user model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   username: {type: String, unique: true, required: true},
   password: {type: String, required: true},
   email:    {type: String, unique: true, required: true},
-  img:      {data: Buffer, contentType: String},
+  img:      {type: String, default: "../assets/images/nerd.png"},
   bio:      {type: String},
   balance:  {type: Number},
   role: {
